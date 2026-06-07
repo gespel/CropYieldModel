0a91e878a680517bf6bf4137d7187a9933f1e961
gespel <sten.heimbrodt@hotmail.de>
2026-06-07 16:56:06 +0200

    check if file already exist for file downloader

--- core/data_downloader.py
+++ core/data_downloader.py
@@ -1,53 +1,34 @@
 import requests
 import tqdm
+import os
+
+def download_files(local_path, start_year, end_year, path, prefix):
+    base_url = f"https://opendata.dwd.de/climate_environment/CDC/grids_germany/daily/hyras_de/{path}/"
+
+    for year in tqdm.tqdm(range(start_year, end_year + 1), desc=f"Downloading {path} files"):
+        file_name = f"{prefix}_hyras_1_{year}_v6-1_de.nc"
+        if os.path.exists(f"{local_path}/{file_name}"):
+            print(f"File already exists: {file_name}, skipping download.")
+            continue
+        file_url = f"{base_url}/{file_name}"
+        response = requests.get(file_url)
+        if response.status_code == 200:
+            with open(f"{local_path}/{file_name}", 'wb') as f:
+                f.write(response.content)
+        else:
+            print(f"Failed to download: {file_name}, Status code: {response.status_code}")
 
 def download_average_temperature_files(local_path):
-    base_url = "https://opendata.dwd.de/climate_environment/CDC/grids_germany/daily/hyras_de/air_temperature_mean/"
-
-    for year in tqdm.tqdm(range(1951, 2024), desc="Downloading temperature files"):
-        file_url = f"{base_url}/tas_hyras_1_{year}_v6-1_de.nc"
-        response = requests.get(file_url)
-        if response.status_code == 200:
-            with open(f"{local_path}/tas_hyras_1_{year}_v6-1_de.nc", 'wb') as f:
-                f.write(response.content)
-        else:
-            print(f"Failed to download: tas_hyras_1_{year}_v6-1_de.nc, Status code: {response.status_code}")
+    download_files(local_path, 1951, 2023, "air_temperature_mean", "tas")
 
 def download_precipitation_files(local_path):
-    base_url = "https://opendata.dwd.de/climate_environment/CDC/grids_germany/daily/hyras_de/precipitation/"
-
-    for year in tqdm.tqdm(range(1951, 2024), desc="Downloading precipitation files"):
-        file_url = f"{base_url}/pr_hyras_1_{year}_v6-1_de.nc"
-        response = requests.get(file_url)
-        if response.status_code == 200:
-            with open(f"{local_path}/pr_hyras_1_{year}_v6-1_de.nc", 'wb') as f:
-                f.write(response.content)
-        else:
-            print(f"Failed to download: pr_hyras_1_{year}_v6-1_de.nc, Status code: {response.status_code}")
+    download_files(local_path, 1951, 2023, "precipitation", "pr")
 
 def download_min_average_temperature_files(local_path):
-    base_url = "https://opendata.dwd.de/climate_environment/CDC/grids_germany/daily/hyras_de/air_temperature_minimum/"
-
-    for year in tqdm.tqdm(range(1951, 2024), desc="Downloading minimum temperature files"):
-        file_url = f"{base_url}/tasmin_hyras_1_{year}_v6-1_de.nc"
-        response = requests.get(file_url)
-        if response.status_code == 200:
-            with open(f"{local_path}/tasmin_hyras_1_{year}_v6-1_de.nc", 'wb') as f:
-                f.write(response.content)
-        else:
-            print(f"Failed to download: tasmin_hyras_1_{year}_v6-1_de.nc, Status code: {response.status_code}")
+    download_files(local_path, 1951, 2023, "air_temperature_minimum", "tasmin")
 
 def download_max_average_temperature_files(local_path):
-    base_url = "https://opendata.dwd.de/climate_environment/CDC/grids_germany/daily/hyras_de/air_temperature_maximum/"
-
-    for year in tqdm.tqdm(range(1951, 2024), desc="Downloading maximum temperature files"):
-        file_url = f"{base_url}/tasmax_hyras_1_{year}_v6-1_de.nc"
-        response = requests.get(file_url)
-        if response.status_code == 200:
-            with open(f"{local_path}/tasmax_hyras_1_{year}_v6-1_de.nc", 'wb') as f:
-                f.write(response.content)
-        else:
-            print(f"Failed to download: tasmax_hyras_1_{year}_v6-1_de.nc, Status code: {response.status_code}")
+    download_files(local_path, 1951, 2023, "air_temperature_maximum", "tasmax")
 
 if __name__ == "__main__":
     download_average_temperature_files("../.data/")
